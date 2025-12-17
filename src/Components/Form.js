4f6c6a5d9eb81a46a5985133c2709f670f3ea822
Musfiqur Rahman <musfiqurrahmanshahed0608@gmail.com>
2025-12-17 12:25:53 +0600

    second commit of Form handling

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,50 +3,70 @@ import React from 'react';
 export default class From extends React.Component {
     state = {
         title: 'JavaScript',
+        text: 'JavaScript is awasome',
+        library: 'React',
+        isAwesome: true
     };
     handleChange = (event) => {
-        console.log(event.target.value);
-        this.setState({ title: event.target.value });
+
+        // if (event.target.type === 'text') {
+        //     this.setState({ title: event.target.value });
+        // } else if (event.target.type === 'textarea') {
+        //     this.setState({ text: event.target.value });
+        // } else if (event.target.type === 'select-one') {
+        //     this.setState({ library: event.target.value });
+        // } else if (event.target.type === 'checkbox') {
+        //     this.setState({ isAwesome: event.target.value });
+        // } else {
+        //     alert('Something wrong');
+        // }
+        const { type, value } = event.target;
+        this.setState({
+            [type]: value
+        });
+
     };
-    render(){
-        const { title } = this.state;
-        return(
+    handleSubmit = (e) => {
+        const { title, text, library, isAwesome } = this.state;
+        e.preventDefault();
+        console.log('Form submitted');
+        console.log(title + '\n' + text + '\n' + library + '\n' + isAwesome);
+    }
+    render() {
+        const { title, text, library, isAwesome } = this.state;
+        return (
             <div>
-                <form>
-                    <input type="text" placeholder="Enter a task" value={title} onChange={this.handleChange}/>
-                    <button type="submit">Add Task</button>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            placeholder="Enter a task"
+                            value={title}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <br></br>
+                    <br></br>
+                    <textarea value={text} onChange={this.handleChange}/>
+
+                    <br></br>
+                    <br></br>
+                    <select value={library} onChange={this.handleChange}>
+                        <option value="React">React</option>
+                        <option value="Angular">Angular</option>
+                        <option value="Java">Java</option>
+                    </select>
+                    <br></br>
+                    <br></br>
+                    <input type="checkbox" checked={isAwesome} onChange={this.handleChange} />
+                    <br></br>
+                    <br></br>
+
+                    <input type="submit" value="submit" />
+
                 </form>
             </div>
         )
     }
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: '',
-//     };
-//   }
-
-//   handleChange = (event) => {
-//     this.setState({ value: event.target.value });
-//   };
-
-//   handleSubmit = (event) => {
-//     event.preventDefault();
-//     this.props.onSubmit(this.state.value);
-//     this.setState({ value: '' });
-//   };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <input
-//           type="text"
-//           value={this.state.value}
-//           onChange={this.handleChange}
-//           placeholder="Enter a task"
-//         />
-//         <button type="submit">Add Task</button>
-//       </form>
-//     );
-//   }
 }
