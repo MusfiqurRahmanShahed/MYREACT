c2ad95c6439ae1e12bb5f9808f197dae8bf8ec72
Musfiqur Rahman <musfiqurrahmanshahed0608@gmail.com>
2025-12-21 16:37:28 +0600

    change the app file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React from "react";
-import ClickCounter from "./Components/ClickCounter";
-import Counter from "./Components/Counter";
-import Section from "./Components/Section";
-import ThemeContext from "./Contexts/themeContext";
+import Todo from "./Components/Todo";
 class App extends React.Component {
-  state = {
-    theme: "dark",
-  };
-
-  switchTheme = () => {
-    this.setState((prevState) => ({
-      theme: prevState.theme === "dark" ? "light" : "dark",
-    }));
-  };
   render() {
-    const { theme } = this.state;
     return (
       <div>
-
-        <Counter>
-          {(count, incrementCount) => (
-            <ClickCounter count={count} incrementCount={incrementCount} />
-          )}
-        </Counter>
-
-        {/* <Counter>
-        {(count, incrementCount) => (
-          <HoverCounter count={count} incrementCount={incrementCount} />
-
-        )}
-      </Counter> */}
-        <ThemeContext.Provider value={{theme , switchTheme: this.switchTheme}}>
-          {/* <Section theme={theme} /> */}
-          <Section />
-        </ThemeContext.Provider>
-
+        <Todo/>
       </div>
     );
   }
